bc66b8e79de360880b22f847d0d538d48c95cb42
Arvin Wiyono <arvinwiyono@gmail.com>
2016-05-04 22:12:30 +1000

    Clean up code and more error handlings

--- image_coding.m
+++ image_coding.m
@@ -1,4 +1,10 @@
-function y = image_coding(imgpath, thres)
+function y = image_coding(imgpath, thres, block_size)
+
+% Validate block size
+if mod(block_size, 2) > 0 
+    error('block_size must be 2^n!');
+end
+
 % Read image
 try
     % If image is RGB
@@ -7,30 +13,42 @@ catch
     % If image is already gray
     gray_image = imread(imgpath);
 end
-    
-% Perform segmentation
+  
+% Get width and height of image
 [width, height] = size(gray_image);
-loop = width/8;
 
-if width == height
-    string = strcat('Width and height are equal. Divide picture into: ', int2str(loop), 'x', int2str(loop), ' blocks of 64 pixels!');
-    disp(string);
-    param(1:1, 1:loop) = 8;
-    blocks = mat2cell(gray_image, param, param);
-else
+% Validate width and height
+if width ~= height
     error('Width and height must be equal!');
 end
 
+% Validate that picture is divisible by block_size
+if mod(width, block_size) == 0
+    loop = width/block_size;
+else
+    error(strcat('Width or height of picture is not divisible by (', int2str(block_size), ')!'));
+end
+
+% Perform segmentation
+string = strcat('Divide picture into: (', int2str(loop), 'x', int2str(loop), ') blocks of (', int2str(block_size.^2),') pixels!');
+disp(string);
+param(1:1, 1:loop) = block_size;
+blocks = mat2cell(gray_image, param, param);
+
+% Set threshold
 threshold = thres;
 
-% Run dct2 algorithm on every blockB{}
 [width, height] = size(blocks);
 for i = 1:width
    for j = 1:height
+      % Run dct2 algorithm on every block{}
       current = blocks{i,j};
       current = dct2(current);
+      
+      % Quantize with threshold
       current(abs(current) < threshold) = 0;
-%       Convert it back using inverse dct2
+      
+      % Convert it back using inverse dct2
       blocks{i,j} = idct2(current);
    end
 end
@@ -38,7 +56,6 @@ end
 compressed = cell2mat(blocks);
 
 % Display original and compressed pictures
-% Now try to run inverse dct2
 figure('Name', 'Original Picture');
 imshow(gray_image);
 figure('Name', 'Compressed Picture');
